be031321091fa98ba414a0d748f84860b794d9ed
Christina Knudsen <christina.knudsen@innovatec.net>
2020-06-04 09:20:42 +0200

    restored

--- App_rosa/app/src/main/java/com/example/app_rosa/MainActivity.java
+++ App_rosa/app/src/main/java/com/example/app_rosa/MainActivity.java
@@ -1,12 +1,27 @@
 package com.example.app_rosa;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Looper;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.IOException;
+import java.util.UUID;
+
 
 public class MainActivity extends AppCompatActivity {
     //private EditText mDegVal;
@@ -58,6 +73,5 @@ public class MainActivity extends AppCompatActivity {
     public void backward(View view) {
         nav.moveBackward();
     }
-
 
 }
